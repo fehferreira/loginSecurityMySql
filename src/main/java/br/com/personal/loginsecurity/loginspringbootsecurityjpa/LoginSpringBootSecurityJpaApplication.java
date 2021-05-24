baad74dc02da9a2378f0e65c36f14de55fbb74c7
Felipe <felipe.ferreira1003@gmail.com>
2021-05-24 15:10:44 -0300

    Starting a login project with Spring Boot / Security

---
+++ src/main/java/br/com/personal/loginsecurity/loginspringbootsecurityjpa/LoginSpringBootSecurityJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.personal.loginsecurity.loginspringbootsecurityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSpringBootSecurityJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginSpringBootSecurityJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/personal/loginsecurity/loginspringbootsecurityjpa/LoginSpringBootSecurityJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.personal.loginsecurity.loginspringbootsecurityjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSpringBootSecurityJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
