83c7c39936cba2c43c0181dfa43821dd3d7f44bd
Felipe <felipe.ferreira1003@gmail.com>
2021-05-26 14:13:57 -0300

    Create LoginController class and develop login and registration request

---
+++ src/main/java/br/com/personal/loginsecurity/securityMySql/controller/LoginController.java
@@ -0,0 +1,34 @@
+package br.com.personal.loginsecurity.securityMySql.controller;
+
+import br.com.personal.loginsecurity.securityMySql.model.User;
+import br.com.personal.loginsecurity.securityMySql.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public ModelAndView registration(){
+        ModelAndView modelAndView = new ModelAndView();
+        User user = new User();
+        modelAndView.addObject("user", user);
+        modelAndView.setViewName("registration");
+        return modelAndView;
+    }
+
+
+}
